58eba89d08ed7d8fd817fd618f176b5fae8da3e6
maxmcd <max.t.mcdonnell@gmail.com>
2020-08-02 21:22:01 -0400

    compatability fix

--- pkg/bramblescript/if_err_test.go
+++ pkg/bramblescript/if_err_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestIfErr(t *testing.T) {
 	tests := []scriptTest{
-		{script: `b=cmd("ls", "-notathing").if_err("echo", "hi").stdout()`,
+		{script: `b=cmd("ls", "notathing").if_err("echo", "hi").stdout()`,
 			returnValue: `"hi\n"`},
-		{script: `b=cmd("ls", "-notathing").if_err("echo", "hi").stdout()`,
+		{script: `b=cmd("ls", "notathing").if_err("echo", "hi").stdout()`,
 			returnValue: `"hi\n"`},
 	}
 	runTest(t, tests)
