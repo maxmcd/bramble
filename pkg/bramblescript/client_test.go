1ce53538ceba0c04752360f12d2dca94305d68c4
maxmcd <max.t.mcdonnell@gmail.com>
2020-08-21 15:20:58 -0400

    more tests and fix for type conversion
    Former-commit-id: df5e82e11f000b3ae15157f7372d1ea8d031acd8

--- pkg/bramblescript/client_test.go
+++ pkg/bramblescript/client_test.go
@@ -8,6 +8,12 @@ func TestCmdClient(t *testing.T) {
 	tests := []scriptTest{
 		{script: "cmd.cd('..');b=cmd('pwd').combined_output().strip().endswith('pkg')",
 			returnValue: "True"},
+		{script: "b=cmd",
+			returnValue: "<built-in function cmd>"},
+		{script: "b=''.join(dir(cmd))",
+			returnValue: `"cddebug"`},
+		{script: "cmd.cd()",
+			errContains: "missing argument"},
 	}
 	runTest(t, tests)
 }
