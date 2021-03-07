b7d13a2d094104a2d3a095493281cdb06931ba98
maxmcd <max.t.mcdonnell@gmail.com>
2021-03-07 11:22:01 -0500

    real progress w/ Go

--- lib/go/build.sh
+++ lib/go/build.sh
@@ -5,15 +5,16 @@ export PATH=$stdenv/bin:$busybox/bin
 export LD_LIBRARY_PATH=$stdenv/lib
 
 mkdir -p /var/tmp
+mkdir /tmp
 
 cp -r $go1_4/go .
+include=$(pwd)/go/include
 cd ./go/src
 ls -lah ./cmd/dist
-export GO_CCFLAGS="-I$stdenv/include-glibc"
-export CC="gcc -I$stdenv/include-glibc -Wl,-rpath=$stdenv/lib "
-export GOROOT_BOOTSTRAP=$(pwd)
+export GO_CCFLAGS="-L $stdenv/lib -I $include -I $stdenv/include-glibc -I $stdenv/include -Wl,-rpath=$stdenv/lib -Wl,--dynamic-linker=$stdenv/lib/ld-linux-x86-64.so.2 "
+export CC="gcc -L $stdenv/lib -I $include -I $stdenv/include-glibc -I $stdenv/include -Wl,-rpath=$stdenv/lib -Wl,--dynamic-linker=$stdenv/lib/ld-linux-x86-64.so.2 "
+# export GOROOT_BOOTSTRAP=$(pwd)
 export CGO_ENABLED="0"
-which sh
 sed -i 's/set -e/set -ex/g' ./make.bash
 # cat ./make.bash
 bash ./make.bash
