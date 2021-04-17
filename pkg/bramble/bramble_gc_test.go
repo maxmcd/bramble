15fccc7908a695037bbc140b2ef77f94a0c79254
maxmcd <max.t.mcdonnell@gmail.com>
2021-04-17 19:05:35 -0400

    fix tests

--- pkg/bramble/bramble_gc_test.go
+++ pkg/bramble/bramble_gc_test.go
@@ -38,7 +38,7 @@ def ok():
 
 func TestDependency(t *testing.T) {
 	tp := cachedProj.Copy()
-	// t.Cleanup(tp.Cleanup)
+	t.Cleanup(tp.Cleanup)
 	if err := ioutil.WriteFile(
 		filepath.Join(tp.projectPath, "./dep.bramble"),
 		[]byte(`
