a4191fae019102381b3e2efb7a06715206346205
maxmcd <max.t.mcdonnell@gmail.com>
2021-09-07 21:41:45 -0400

    comment out bramble_test for now

--- pkg/bramble_test/test_test.go
+++ pkg/bramble_test/test_test.go
@@ -1,86 +1,86 @@
 package bramble_test
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"testing"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"os"
+// 	"testing"
 
-	"github.com/maxmcd/bramble/pkg/bramble"
-	"github.com/maxmcd/bramble/pkg/fileutil"
-	"github.com/maxmcd/bramble/pkg/starutil"
-)
+// 	"github.com/maxmcd/bramble/pkg/bramble"
+// 	"github.com/maxmcd/bramble/pkg/fileutil"
+// 	"github.com/maxmcd/bramble/pkg/starutil"
+// )
 
-type TestProject struct {
-	bramblePath string
-	projectPath string
-}
+// type TestProject struct {
+// 	bramblePath string
+// 	projectPath string
+// }
 
-var cachedProj *TestProject
+// var cachedProj *TestProject
 
-func TestMain(m *testing.M) {
-	var err error
-	cachedProj, err = newTestProject()
-	if err != nil {
-		fmt.Printf("%+v", err)
-		panic(starutil.AnnotateError(err))
-	}
-	exitVal := m.Run()
-	cachedProj.Cleanup()
-	os.Exit(exitVal)
-}
+// func TestMain(m *testing.M) {
+// 	var err error
+// 	cachedProj, err = newTestProject()
+// 	if err != nil {
+// 		fmt.Printf("%+v", err)
+// 		panic(starutil.AnnotateError(err))
+// 	}
+// 	exitVal := m.Run()
+// 	cachedProj.Cleanup()
+// 	os.Exit(exitVal)
+// }
 
-func (tp *TestProject) Copy() *TestProject {
-	out := TestProject{
-		bramblePath: fileutil.TestTmpDir(nil),
-		projectPath: fileutil.TestTmpDir(nil),
-	}
-	if err := fileutil.CopyDirectory(tp.bramblePath, out.bramblePath); err != nil {
-		panic(err)
-	}
-	if err := fileutil.CopyDirectory(tp.projectPath, out.projectPath); err != nil {
-		panic(err)
-	}
-	return &out
-}
-func (tp *TestProject) Bramble() *bramble.Bramble {
-	b, err := bramble.NewBramble(tp.projectPath,
-		bramble.OptionNoRoot,
-		bramble.OptionBramblePath(tp.bramblePath))
-	if err != nil {
-		panic(err)
-	}
-	// b.noRoot = true
-	return b
-}
+// func (tp *TestProject) Copy() *TestProject {
+// 	out := TestProject{
+// 		bramblePath: fileutil.TestTmpDir(nil),
+// 		projectPath: fileutil.TestTmpDir(nil),
+// 	}
+// 	if err := fileutil.CopyDirectory(tp.bramblePath, out.bramblePath); err != nil {
+// 		panic(err)
+// 	}
+// 	if err := fileutil.CopyDirectory(tp.projectPath, out.projectPath); err != nil {
+// 		panic(err)
+// 	}
+// 	return &out
+// }
+// func (tp *TestProject) Bramble() *bramble.Bramble {
+// 	b, err := bramble.NewBramble(tp.projectPath,
+// 		bramble.OptionNoRoot,
+// 		bramble.OptionBramblePath(tp.bramblePath))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	// b.noRoot = true
+// 	return b
+// }
 
-func (tp *TestProject) Cleanup() {
-	_ = os.RemoveAll(tp.bramblePath)
-	_ = os.RemoveAll(tp.projectPath)
-}
+// func (tp *TestProject) Cleanup() {
+// 	_ = os.RemoveAll(tp.bramblePath)
+// 	_ = os.RemoveAll(tp.projectPath)
+// }
 
-func newTestProject() (*TestProject, error) {
-	// Write files
-	bramblePath := fileutil.TestTmpDir(nil)
-	projectPath := fileutil.TestTmpDir(nil)
+// func newTestProject() (*TestProject, error) {
+// 	// Write files
+// 	bramblePath := fileutil.TestTmpDir(nil)
+// 	projectPath := fileutil.TestTmpDir(nil)
 
-	if err := fileutil.CopyDirectory("./testdata", projectPath); err != nil {
-		return nil, err
-	}
+// 	if err := fileutil.CopyDirectory("./testdata", projectPath); err != nil {
+// 		return nil, err
+// 	}
 
-	// Init bramble
-	b, err := bramble.NewBramble(projectPath,
-		bramble.OptionNoRoot,
-		bramble.OptionBramblePath(bramblePath))
-	if err != nil {
-		return nil, err
-	}
-	ctx := context.Background()
-	if _, _, err = b.Build(ctx, []string{":fetch_busybox"}); err != nil {
-		return nil, err
-	}
-	return &TestProject{
-		bramblePath: bramblePath,
-		projectPath: projectPath,
-	}, nil
-}
+// 	// Init bramble
+// 	b, err := bramble.NewBramble(projectPath,
+// 		bramble.OptionNoRoot,
+// 		bramble.OptionBramblePath(bramblePath))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	ctx := context.Background()
+// 	if _, _, err = b.Build(ctx, []string{":fetch_busybox"}); err != nil {
+// 		return nil, err
+// 	}
+// 	return &TestProject{
+// 		bramblePath: bramblePath,
+// 		projectPath: projectPath,
+// 	}, nil
+// }
