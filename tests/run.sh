a1769925d3ccc67b0ba8fb7c2ab9702432dcc4d6
maxmcd <max.t.mcdonnell@gmail.com>
2021-10-23 11:59:48 -0400

    coverage first

--- tests/run.sh
+++ tests/run.sh
@@ -4,10 +4,6 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 
 cd ..
 
-gotestsum -- \
-    -coverpkg=./... -coverprofile=coverage.txt -covermode=atomic -race -v ./...
+env BRAMBLE_INTEGRATION_TEST=truthy gotestsum -- -coverpkg=./... -coverprofile=coverage.txt -v ./...
 bash <(curl -s https://codecov.io/bash)
-
-env BRAMBLE_INTEGRATION_TEST=truthy gotestsum -- \
-    -coverpkg=./... -coverprofile=coverage.txt -v ./internal/command/
-bash <(curl -s https://codecov.io/bash)
+gotestsum -- -race -v ./...
