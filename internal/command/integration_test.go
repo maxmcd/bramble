34fe2f37a7059c3e56529f73054bbb2dcc209da7
maxmcd <max.t.mcdonnell@gmail.com>
2021-10-23 12:02:57 -0400

    comment out defunct integration tests

--- internal/command/integration_test.go
+++ internal/command/integration_test.go
@@ -1,23 +1,15 @@
 package command
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
 	"runtime"
-	"strings"
-	"sync"
 	"testing"
-	"time"
 
 	"github.com/maxmcd/bramble/internal/types"
 	"github.com/opencontainers/runc/libcontainer"
 	_ "github.com/opencontainers/runc/libcontainer/nsenter"
-	"github.com/stretchr/testify/assert"
 )
 
 // init runs the libcontainer initialization code because of the busybox style needs
@@ -47,90 +39,91 @@ func initIntegrationTest(t *testing.T) {
 		t.Skip("skipping integration tests unless BRAMBLE_INTEGRATION_TEST is set")
 	}
 }
-func TestRun(t *testing.T) {
-	initIntegrationTest(t)
-	runRun := func(module string, args []string) (output string, exitCode int) {
-		cmd := exec.Command("bramble", append([]string{"run", module}, args...)...)
-		o, _ := cmd.CombinedOutput()
-		fmt.Println(string(o))
-		return string(o), cmd.ProcessState.ExitCode()
-	}
 
-	type test struct {
-		name   string
-		module string
-		args   []string
+// func TestRun(t *testing.T) {
+// 	initIntegrationTest(t)
+// 	runRun := func(module string, args []string) (output string, exitCode int) {
+// 		cmd := exec.Command("bramble", append([]string{"run", module}, args...)...)
+// 		o, _ := cmd.CombinedOutput()
+// 		fmt.Println(string(o))
+// 		return string(o), cmd.ProcessState.ExitCode()
+// 	}
 
-		outputContains   string
-		expectedExitcode int
-	}
-	for _, tt := range []test{
-		// Removed until it's reproducible
-		// {
-		// 	name:           "go run",
-		// 	module:         "../../lib/go:bootstrap",
-		// 	args:           []string{"go", "run", "testdata/main.go"},
-		// 	outputContains: "hello world",
-		// },
-		// {
-		// 	name:             "go run w/ exit code",
-		// 	module:           "../../lib/go:bootstrap",
-		// 	args:             []string{"go", "run", "testdata/main.go", "-exit-code", "2"},
-		// 	outputContains:   "exit status 2",
-		// 	expectedExitcode: 1, // go run will exit w/ 1 and print the non-1 exit code
-		// },
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			output, exitCode := runRun(tt.module, tt.args)
-			assert.Equal(t, tt.expectedExitcode, exitCode)
-			assert.Contains(t, output, tt.outputContains)
-		})
-	}
-}
+// 	type test struct {
+// 		name   string
+// 		module string
+// 		args   []string
 
-type lockWriter struct {
-	lock   sync.Mutex
-	writer io.Writer
-}
+// 		outputContains   string
+// 		expectedExitcode int
+// 	}
+// 	for _, tt := range []test{
+// 		// Removed until it's reproducible
+// 		// {
+// 		// 	name:           "go run",
+// 		// 	module:         "../../lib/go:bootstrap",
+// 		// 	args:           []string{"go", "run", "testdata/main.go"},
+// 		// 	outputContains: "hello world",
+// 		// },
+// 		// {
+// 		// 	name:             "go run w/ exit code",
+// 		// 	module:           "../../lib/go:bootstrap",
+// 		// 	args:             []string{"go", "run", "testdata/main.go", "-exit-code", "2"},
+// 		// 	outputContains:   "exit status 2",
+// 		// 	expectedExitcode: 1, // go run will exit w/ 1 and print the non-1 exit code
+// 		// },
+// 	} {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			output, exitCode := runRun(tt.module, tt.args)
+// 			assert.Equal(t, tt.expectedExitcode, exitCode)
+// 			assert.Contains(t, output, tt.outputContains)
+// 		})
+// 	}
+// }
 
-func (lw *lockWriter) Write(p []byte) (n int, err error) {
-	lw.lock.Lock()
-	defer lw.lock.Unlock()
-	return lw.writer.Write(p)
-}
+// type lockWriter struct {
+// 	lock   sync.Mutex
+// 	writer io.Writer
+// }
 
-func TestDep_handler(t *testing.T) {
-	initIntegrationTest(t)
-	cmd := exec.Command("bramble", "server")
-	buf := &bytes.Buffer{}
-	lw := &lockWriter{writer: io.MultiWriter(os.Stdout, buf)}
-	cmd.Stdout = lw
-	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-	for {
-		lw.lock.Lock()
-		if strings.Contains(buf.String(), "localhost") {
-			lw.writer = os.Stdout
-			lw.lock.Unlock()
-			break
-		}
-		lw.lock.Unlock()
-		time.Sleep(time.Millisecond * 100)
-	}
+// func (lw *lockWriter) Write(p []byte) (n int, err error) {
+// 	lw.lock.Lock()
+// 	defer lw.lock.Unlock()
+// 	return lw.writer.Write(p)
+// }
 
-	t.Cleanup(func() { _ = cmd.Process.Kill() })
+// func TestDep_handler(t *testing.T) {
+// 	initIntegrationTest(t)
+// 	cmd := exec.Command("bramble", "server")
+// 	buf := &bytes.Buffer{}
+// 	lw := &lockWriter{writer: io.MultiWriter(os.Stdout, buf)}
+// 	cmd.Stdout = lw
+// 	cmd.Stderr = os.Stderr
+// 	if err := cmd.Start(); err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for {
+// 		lw.lock.Lock()
+// 		if strings.Contains(buf.String(), "localhost") {
+// 			lw.writer = os.Stdout
+// 			lw.lock.Unlock()
+// 			break
+// 		}
+// 		lw.lock.Unlock()
+// 		time.Sleep(time.Millisecond * 100)
+// 	}
 
-	{
-		cmd := exec.Command("bramble", "publish", "github.com/maxmcd/busybox")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
+// 	t.Cleanup(func() { _ = cmd.Process.Kill() })
+
+// 	{
+// 		cmd := exec.Command("bramble", "publish", "github.com/maxmcd/busybox")
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Stderr = os.Stderr
+// 		if err := cmd.Run(); err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	}
+// }
 
 func TestNative(t *testing.T) {
 	initIntegrationTest(t)
