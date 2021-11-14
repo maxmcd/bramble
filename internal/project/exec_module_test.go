542800990135f6ffd08c76b5768dfa07de7e76f8
maxmcd <max.t.mcdonnell@gmail.com>
2021-11-14 11:25:50 -0500

    add back test

--- internal/project/exec_module_test.go
+++ internal/project/exec_module_test.go
@@ -1,57 +1,65 @@
 package project
 
-// func TestExecModule(t *testing.T) {
-// 	project, err := NewProject("./testdata/project")
-// 	require.NoError(t, err)
-// 	type output struct {
-// 		output []string
-// 		all    []string
-// 	}
-// 	tests := []struct {
-// 		name       string
-// 		args       []string
-// 		wantOutput output
-// 		wantErr    bool
-// 	}{
-// 		{
-// 			args: []string{":chain"},
-// 			wantOutput: output{
-// 				output: []string{"c"},
-// 				all:    []string{"a", "b", "c"},
-// 			},
-// 		},
-// 		{
-// 			args: []string{":foo"},
-// 			wantOutput: output{
-// 				output: []string{"name"},
-// 				all:    []string{"example.com", "name"},
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			gotOutput, err := project.ExecModule(context.Background(), ExecModuleInput{
-// 				Command:   "build",
-// 				Arguments: tt.args,
-// 			})
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("ExecModule() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
+import (
+	"context"
+	"reflect"
+	"sort"
+	"testing"
 
-// 			reducedOutput := output{}
-// 			for _, drv := range gotOutput.AllDerivations {
-// 				reducedOutput.all = append(reducedOutput.all, drv.Name)
-// 			}
-// 			for _, drv := range gotOutput.Output {
-// 				reducedOutput.output = append(reducedOutput.output, drv.Name)
-// 			}
-// 			sort.Strings(reducedOutput.all)
-// 			sort.Strings(reducedOutput.output)
+	"github.com/stretchr/testify/require"
+)
 
-// 			if !reflect.DeepEqual(reducedOutput, tt.wantOutput) {
-// 				t.Errorf("ExecModule() = %v, want %v", reducedOutput, tt.wantOutput)
-// 			}
-// 		})
-// 	}
-// }
+func TestExecModule(t *testing.T) {
+	project, err := NewProject("./testdata/project")
+	require.NoError(t, err)
+	type output struct {
+		output []string
+		all    []string
+	}
+	tests := []struct {
+		name       string
+		arg        string
+		wantOutput output
+		wantErr    bool
+	}{
+		{
+			arg: ":chain",
+			wantOutput: output{
+				output: []string{"c"},
+				all:    []string{"a", "b", "c"},
+			},
+		},
+		{
+			arg: ":foo",
+			wantOutput: output{
+				output: []string{"name"},
+				all:    []string{"example.com", "name"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOutput, err := project.ExecModule(context.Background(), ExecModuleInput{
+				Module: tt.arg,
+			})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExecModule() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			reducedOutput := output{}
+			for _, drv := range gotOutput.AllDerivations {
+				reducedOutput.all = append(reducedOutput.all, drv.Name)
+			}
+			for _, drv := range gotOutput.Output {
+				reducedOutput.output = append(reducedOutput.output, drv.Name)
+			}
+			sort.Strings(reducedOutput.all)
+			sort.Strings(reducedOutput.output)
+
+			if !reflect.DeepEqual(reducedOutput, tt.wantOutput) {
+				t.Errorf("ExecModule() = %v, want %v", reducedOutput, tt.wantOutput)
+			}
+		})
+	}
+}
